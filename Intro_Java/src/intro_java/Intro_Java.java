1f9a99f675a967de39d1b068932bfc33299d3513
Ricardo Henriques <ricardohenriques7410@gmail.com>
2022-09-15 09:23:40 +0100

    Converter moedas

--- Intro_Java/src/intro_java/Intro_Java.java
+++ Intro_Java/src/intro_java/Intro_Java.java
@@ -49,6 +49,14 @@ public class Intro_Java {
     }
 
     private static void converteMoedas() {
+        float moeda;
+        System.out.println("Insira um valor em euros");
+        moeda = ler.nextFloat();
+        System.out.println(moeda+ " (€) equivale a:\n"+
+                +(moeda*5.15)+" (R$)\n"+
+                +(moeda)+" (USD)\n"+
+                +(moeda*7.52)+" (kn)\n"+
+                +(moeda*143.24)+" (¥)");
     }
     
 }
